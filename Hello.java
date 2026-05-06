f06fa6e865f88e1048ee362d231e669f8080b0dc
utomiobuh <greatobuh123@gmail.com>
2026-05-06 16:22:24 +0100

    Hello Program

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+	public static void main(String[] args){
+		System.out.println("Hello Everyone, I am Learning Java");
+		System.out.print("Oh wow,Learning Java is Annoying. ");
+		System.out.print("Hurray, I dey vex. ");
+		System.out.printf("Java Was Created In %d",1992);
+		System.out.printf("Java Was Free for %c%.2f", '$', 0.0000);
+		System.out.printf("Do you love Java %c %b",'?',true);
+		System.out.printf(Yes Learning %s is annoying","Java");
+		}
+		
+}
